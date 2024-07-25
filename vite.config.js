f564aa8394e8da59a281a90cadd383680195c448
Ninjaparade <yazjallad@gmail.com>
2024-07-25 11:10:59 -0700

    add frontend stuff

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,56 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import { run } from 'vite-plugin-run'
+import Unimport from 'unimport/unplugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/js/app.ts'],
+      refresh: true,
+    }),
+    Unimport.vite({
+      imports: [
+        { name: 'default', as: 'axios', from: 'axios' },
+      ],
+      addons: {
+        vueTemplate: true,
+      },
+      dirs: [
+        'resources/config/*',
+        'resources/utils/*',
+        'resources/composables/*',
+      ],
+    }),
+    run([
+      // Watch Data, DTOs, and Enums
+      {
+        name: 'Generate TypeScript Types',
+        run: ['php', 'artisan', 'typescript:transform'],
+        pattern: [
+          '+(app|src)/**/*Data.php',
+          '+(app|src)/**/Enums/*.php',
+          '+(app|src)/**/Middleware/HandleInertiaRequests.php',
+        ],
+      },
+    ]),
+  ],
+
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.vue_vue_type_style_index_0_lang')) {
+            return `assets/${assetInfo.name.slice(0, -32)}-[hash:8].js`
+          }
+          else if (assetInfo.name?.endsWith('.vue_vue_type_script_setup_true_lang')) {
+            return `assets/${assetInfo.name.slice(0, -36)}-[hash:8].js`
+          }
+          else {
+            return 'assets/[name]-[hash:8].js'
+          }
+        },
+      },
+    },
+  },
+})
